b963c9ead296a2334e35545d899f62aafd6e7693
Matias Korhonen <matias@kiskolabs.com>
2015-11-02 10:37:53 +0200

    Removes unused git aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -18,12 +18,8 @@ alias glff='git pull --ff-only'
 alias gp='git push'
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
 alias gco='git checkout'
-alias gb='git branch'
 alias gs='git status'
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gx='gitx --all'
 alias glog="git log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %an' --abbrev-commit --date=relative"
 
 # rails
