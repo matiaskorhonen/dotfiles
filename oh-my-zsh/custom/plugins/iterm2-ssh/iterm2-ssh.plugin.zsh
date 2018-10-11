df21456a8701bc5b5928fc520dbeb72a8fbd378f
Matias Korhonen <matias@kiskolabs.com>
2018-10-11 09:15:51 +0300

    Fixes the ZSH traps
    Without this weird stuff happens
    
    https://superuser.com/questions/1178934/iterm2-ctrl-c-doesnt-work-sometimes

--- oh-my-zsh/custom/plugins/iterm2-ssh/iterm2-ssh.plugin.zsh
+++ oh-my-zsh/custom/plugins/iterm2-ssh/iterm2-ssh.plugin.zsh
@@ -1,3 +1,7 @@
+function tab_reset_trap {
+  trap - INT
+}
+
 function tabc() {
   NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
   # if you have trouble with this, change
@@ -6,29 +10,34 @@ function tabc() {
 }
 
 function tab-reset() {
-    NAME="Default"
-    echo -e "\033]50;SetProfile=$NAME\a"
+  NAME="Default"
+  echo -e "\033]50;SetProfile=$NAME\a"
 }
 
 function ssh() {
-    if [[ -n "$ITERM_SESSION_ID" ]]; then
-        trap "tab-reset" INT EXIT
-        tabc SSH
-        # if [[ "$*" =~ "production*" ]]; then
-        #     tabc Production
-        # elif [[ "$*" =~ "staging*" ]]; then
-        #     tabc Staging
-        # else
-        #     tabc SSH
-        # fi
-    fi
-    command ssh "$@"
+  if [[ -n "$ITERM_SESSION_ID" ]]; then
+    trap "tab-reset" INT EXIT
+    tab_reset_trap
+    tabc SSH
+
+    # Use an if like this to change the profile based on the hostname
+    #
+    # if [[ "$*" =~ "production*" ]]; then
+    #     tabc Production
+    # elif [[ "$*" =~ "staging*" ]]; then
+    #     tabc Staging
+    # else
+    #     tabc SSH
+    # fi
+  fi
+  command ssh "$@"
 }
 
 function heroku() {
   if [[ "$@" =~ "console*" || "$@" =~ "run bash*" ]]; then
     trap "tab-reset" INT EXIT
-    tabc SSH
+    tab_reset_trap
+    tabc Heroku
   fi
   command heroku "$@"
 }
