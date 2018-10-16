12347e4e3fc24c21384d9ce5b5c33e651faa4f2c
Matias Korhonen <matias@kiskolabs.com>
2018-10-16 15:07:19 +0300

    Small tweaks

--- oh-my-zsh/custom/plugins/iterm2-ssh/iterm2-ssh.plugin.zsh
+++ oh-my-zsh/custom/plugins/iterm2-ssh/iterm2-ssh.plugin.zsh
@@ -1,43 +1,45 @@
-function tab_reset_trap {
+function _tab-reset-trap() {
   trap - INT
 }
 
-function tabc() {
+function _tabc() {
   NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
   # if you have trouble with this, change
   # "Default" to the name of your default theme
   echo -e "\033]50;SetProfile=$NAME\a"
 }
 
-function tab-reset() {
+function _tab-reset() {
   NAME="Default"
   echo -e "\033]50;SetProfile=$NAME\a"
 }
 
 function ssh() {
   if [[ -n "$ITERM_SESSION_ID" ]]; then
-    trap "tab-reset" INT EXIT
-    tab_reset_trap
-    tabc SSH
+    trap "_tab-reset" INT EXIT
+    _tab-reset-trap
+    _tabc SSH
 
     # Use an if like this to change the profile based on the hostname
     #
     # if [[ "$*" =~ "production*" ]]; then
-    #     tabc Production
+    #     _tabc Production
     # elif [[ "$*" =~ "staging*" ]]; then
-    #     tabc Staging
+    #     _tabc Staging
     # else
-    #     tabc SSH
+    #     _tabc SSH
     # fi
   fi
   command ssh "$@"
 }
 
 function heroku() {
-  if [[ "$@" =~ "console*" || "$@" =~ "run bash*" ]]; then
-    trap "tab-reset" INT EXIT
-    tab_reset_trap
-    tabc Heroku
+  if [[ -n "$ITERM_SESSION_ID" ]]; then
+    if [[ "$@" =~ "console*" || "$@" =~ "run bash*" ]]; then
+      trap "_tab-reset" INT EXIT
+      _tab-reset-trap
+      _tabc Heroku
+    fi
   fi
   command heroku "$@"
 }
