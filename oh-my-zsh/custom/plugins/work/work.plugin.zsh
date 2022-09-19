178b8b8dd11c433465b660771038b576fb398d3b
Matias Korhonen <matias.korhonen@kaikuhealth.com>
2022-09-19 13:17:36 +0300

    Update the bootkaari script

--- oh-my-zsh/custom/plugins/work/work.plugin.zsh
+++ oh-my-zsh/custom/plugins/work/work.plugin.zsh
@@ -5,7 +5,6 @@ function bootkaari() {
   HOLVIKAARI_SITE="ci"
   HOLVIKAARI_PATH="$HOME/Code/work/holvikaari"
   cd "$HOLVIKAARI_PATH"
-  bundle _2.1.4_ install && yarn install && yarn install --cwd=./frontend
   osascript <<END
   tell application "iTerm"
       activate
@@ -16,8 +15,8 @@ function bootkaari() {
           split vertically with default profile
       end tell
       set T to W's current tab
-      write T's session 1 text "cd '$HOLVIKAARI_PATH' && SITE=$HOLVIKAARI_SITE bundle exec rails s"
-      write T's session 2 text "cd '$HOLVIKAARI_PATH' && SITE=$HOLVIKAARI_SITE yarn --cwd=./frontend start"
+      write T's session 1 text "cd \"$HOLVIKAARI_PATH\"\nbundle install\nSITE=$HOLVIKAARI_SITE bundle exec rails s"
+      write T's session 2 text "cd \"$HOLVIKAARI_PATH\"\nyarn install\nyarn install --cwd=./frontend\nSITE=$HOLVIKAARI_SITE yarn --cwd=./frontend start"
   end tell
 END
 }
