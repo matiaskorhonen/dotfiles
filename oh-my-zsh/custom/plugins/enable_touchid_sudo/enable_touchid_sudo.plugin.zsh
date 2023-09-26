6902af75174fb47f4aad1fa410a3c54a61e1cadb
Matias Korhonen <me@matiaskorhonen.fi>
2023-09-26 20:57:04 +0300

    Update the sudo TouchD function for Sonoma

--- oh-my-zsh/custom/plugins/enable_touchid_sudo/enable_touchid_sudo.plugin.zsh
+++ oh-my-zsh/custom/plugins/enable_touchid_sudo/enable_touchid_sudo.plugin.zsh
@@ -1,30 +1,11 @@
-# Based on:
-# https://gist.github.com/RichardBronosky/31660eb4b0f0ba5e673b9bc3c9148a70
-
-# Use TouchID for sudo on modern MacBook Pro machines
-# This script adds a single line to the top of the PAM configuration for sudo
-# See: https://apple.stackexchange.com/q/259093/41827 for more info.
+# Use TouchID for sudo on modern MacBook Pro machines (>= macOS Sonoma)
+#
+# See: https://sixcolors.com/post/2023/08/in-macos-sonoma-touch-id-for-sudo-can-survive-updates/
 
 unalias enable_touchid_sudo 2>/dev/null
 function enable_touchid_sudo() {
   sudo bash -eu <<'EOF'
-  file=/etc/pam.d/sudo
-  # A backup file will be created with the pattern /etc/pam.d/.sudo.1
-  # (where 1 is the number of backups, so that rerunning this doesn't make you lose your original)
-  bak=$(dirname $file)/.$(basename $file).$(echo $(ls $(dirname $file)/{,.}$(basename $file)* | wc -l))
-  cp $file $bak
-  awk -v is_done='pam_tid' -v rule='auth       sufficient     pam_tid.so' '
-  {
-    # $1 is the first field
-    # !~ means "does not match pattern"
-    if($1 !~ /^#.*/){
-      line_number_not_counting_comments++
-    }
-    # $0 is the whole line
-    if(line_number_not_counting_comments==1 && $0 !~ is_done){
-      print rule
-    }
-    print
-  }' > $file < $bak
+  cd /etc/pam.d
+  sed "s/#auth/auth/g" sudo_local.template > sudo_local
 EOF
 }
